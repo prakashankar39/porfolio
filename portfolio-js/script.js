e9248e1e53617daebcaa5d5cce4fc5ee1ed62edf
Prakash Shankar <prakashankar39@Prakashs-MacBook-Pro.local>
2026-05-10 04:01:05 -0500

    update my site

--- portfolio-js/script.js
+++ portfolio-js/script.js
@@ -1,35 +1,16 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        // sticky navbar on scroll script
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
 
-        // scroll-up button show/hide script
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+const fadeElements = document.querySelectorAll('.fade-up');
 
-    // slide-up script
-    $('.scroll-up-btn').click(function() {
-        $('html').animate({ scrollTop: 0 });
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+},{
+  threshold:0.15
+});
 
-    $('.navbar .menu li a').click(function() {
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function() {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+fadeElements.forEach(el=>{
+  observer.observe(el);
 });
